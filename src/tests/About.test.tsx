aeedefb1ce048247cd1e50aadeaaf67cf49fe9a8
Pedro Prudencio <pedroprudencio.pp@gmail.com>
2023-08-16 18:27:25 -0300

    requisito 2

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../pages';
+
+describe('Teste o componente <About.tsx />', () => {
+  test('A página contém as informações sobre a Pokédex:', () => {
+    renderWithRouter(<About />);
+
+    const aboutText = screen.getByText(/This application simulates a Pokédex/i);
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  test('A página contém um heading h2 com o texto About Pokédex:', () => {
+    renderWithRouter(<About />);
+
+    const heading = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(heading).toBeInTheDocument();
+  });
+});
