8c0d76cc7ac838de70ab5d5b9ce51028cb2298f8
Pedro Prudencio <pedroprudencio.pp@gmail.com>
2023-08-16 18:59:07 -0300

    requisito 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,25 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testando o componente NotFound.tsx', () => {
+  test('A página contém um heading h2 com o texto Page requested not found:', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('A página mostra a imagem com o texto alternativo:', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen.getByAltText(
+      /clefairy pushing buttons randomly with text i have no idea what i'm doing/i,
+    );
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute(
+      'src',
+      '/404.gif',
+    );
+  });
+});
