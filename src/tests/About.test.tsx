d0738c1804ea338d21c5104181469b049a551b30
Pedro Prudencio <pedroprudencio.pp@gmail.com>
2023-08-16 18:40:53 -0300

    requisito 2 ok

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -2,7 +2,7 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import { About } from '../pages';
 
-describe('Teste o componente <About.tsx />', () => {
+describe('Testando o componente <About.tsx />', () => {
   test('A página contém as informações sobre a Pokédex:', () => {
     renderWithRouter(<About />);
 
@@ -15,5 +15,24 @@ describe('Teste o componente <About.tsx />', () => {
 
     const heading = screen.getByRole('heading', { name: /about pokédex/i });
     expect(heading).toBeInTheDocument();
+  });
+  test('A página contém dois parágrafos com texto sobre a Pokédex:', () => {
+    renderWithRouter(<About />);
+
+    const paragraph1 = screen.getByText(/This application/i);
+    const paragraph2 = screen.getByText(/One can filter Pokémon /i);
+
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+  test('A página contém a imagem da Pokédex:', () => {
+    renderWithRouter(<About />);
+
+    const pokedexImage = screen.getByRole('img', { name: /pokédex/i });
+    expect(pokedexImage).toBeInTheDocument();
+    expect(pokedexImage).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
   });
 });
