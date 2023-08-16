23de0c7159afd2df356b46077529ad817a9a0459
Pedro Prudencio <pedroprudencio.pp@gmail.com>
2023-08-16 20:25:50 -0300

    requisito 5 corrigido dnv

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -36,37 +36,27 @@ describe('Testando o componente Pokedex.tsx', () => {
   });
   test('A Pokédex tem os botões de filtro:', () => {
     renderWithRouter(<App />);
-
-    // Encontra todos os botões de filtro de tipo
     const typeButtons = screen.getAllByTestId('pokemon-type-button');
 
-    // Verifica se cada botão de filtro de tipo está presente na tela
     typeButtons.forEach((button) => {
       expect(button).toBeInTheDocument();
 
-      // Clique no botão de filtro do tipo
       fireEvent.click(button);
 
-      // Verifica o comportamento da Pokédex de acordo com o tipo selecionado
-      // Certifique-se de ajustar a verificação de acordo com o comportamento da sua aplicação
-      // (por exemplo, verifique se os Pokémon exibidos são do tipo selecionado)
-
-      // Clique no botão de reset (All)
       fireEvent.click(screen.getByText('All'));
 
-      // Verifica se o Pikachu está de volta à tela
       const pikachuName = screen.getByText(/pikachu/i);
       expect(pikachuName).toBeInTheDocument();
     });
-    test('A Pokédex contém um botão para resetar o filtro:', () => {
-      renderWithRouter(<App />);
-      const resetButton = screen.getByText('All');
-      expect(resetButton).toBeInTheDocument();
+  test('A Pokédex contém um botão para resetar o filtro:', () => {
+    renderWithRouter(<App />);
+    const resetButton = screen.getByText('All');
+    expect(resetButton).toBeInTheDocument();
 
-      fireEvent.click(resetButton);
+    fireEvent.click(resetButton);
 
-      const firstPokemon = screen.getByText(/pikachu/i);
-      expect(firstPokemon).toBeInTheDocument();
-    });
+    const firstPokemon = screen.getByText(/pikachu/i);
+    expect(firstPokemon).toBeInTheDocument();
   });
+});
 });
