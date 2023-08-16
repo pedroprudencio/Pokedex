d4e3cdcff6c45f463293c40311935519d8d71f6b
Pedro Prudencio <pedroprudencio.pp@gmail.com>
2023-08-16 16:38:37 -0300

    requisito 1

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o componente App.tsx', () => {
+  test('O topo da aplicação contém um conjunto fixo de links de navegação:', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    const favoriteLink = screen.getByRole('link', { name: /favorite pokémon/i });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+});
