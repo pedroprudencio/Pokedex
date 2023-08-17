f9d477827ab2ae5bc3863dcf05aad1d6f10ce21a
Pedro Prudencio <pedroprudencio.pp@gmail.com>
2023-08-17 19:28:06 -0300

    requisito favorito

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,36 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente FavoritePokemon.tsx', () => {
+  test('exibe mensagem quando não há Pokémon favorito', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={ [] } />);
+
+    const mensagemSemFavoritos = screen.getByText(/No favorite Pokémon found/i);
+    expect(mensagemSemFavoritos).toBeInTheDocument();
+  });
+
+  test('exibe apenas os Pokémon favoritados', () => {
+    const favoritePokemonList = [
+      {
+        id: 25,
+        name: 'Pikachu',
+        type: 'Electric',
+        averageWeight: {
+          value: '6.0',
+          measurementUnit: 'kg',
+        },
+      },
+
+    ];
+
+    renderWithRouter(<FavoritePokemon pokemonList={ favoritePokemonList } />);
+
+    const pokemonsFavoritados = screen.queryAllByTestId('pokemon-type');
+    expect(pokemonsFavoritados.length).toBe(favoritePokemonList.length);
+
+    pokemonsFavoritados.forEach((pokemon) => {
+      expect(pokemon).toHaveTextContent(/Electric/i);
+    });
+  });
+});
